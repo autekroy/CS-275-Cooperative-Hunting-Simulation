bd837ba94e02b69fedd07401e6de2aeb3ddb65eb
Tai-Yi Wu <demundo@s-164-67-211-202.resnet.ucla.edu>
2015-06-05 21:43:17 -0700

    predator update

--- animats.py
+++ animats.py
@@ -4,6 +4,7 @@ import random
 import math
 from enum import Enum
 import numpy as np
+import sys
 
 from pybrain.structure import RecurrentNetwork, FeedForwardNetwork, LinearLayer, SigmoidLayer, FullConnection
 Default_Engery = 100
@@ -36,7 +37,7 @@ class Environment:
 
     # prey
     self.preys = []
-    num_preys = 30
+    num_preys = 2
     for i in range(num_preys):
       p = Prey(random.random() * 360, random.random() * 360)
       self.preys.append(p)
@@ -109,7 +110,7 @@ class Environment:
       step_y = int(math.sin(animat.direction*math.pi / 180) * step)
       animat.touching = self.collision(animat.loc[0] + step_x, animat.loc[1] + step_y, Predator.radius, animat)
       # update
-      animat.update()
+      animat.update(self.predators, self.preys)
 
       # moviing
       animat.loc[0] = step_x + animat.loc[0]
@@ -168,13 +169,26 @@ class Prey:
     self.maxForce = 10
     self.mass = 12
     self.r = self.mass / 2    
-  
+    #for testing
+    self.width = 1000
+    self.height = 700
+    #-----------end of testing
   def update(self, preys):
     self.preyForce(preys)
     self.vel = np.add(self.vel, self.acc)
     self.loc = np.add(self.loc, self.vel)
     self.acc = [0., 0.]
+    '''#for testing
+    if self.loc[0] <= 0:
+      self.loc[0] = self.width
+    elif self.loc[0] > self.width:
+      self.loc[0] = 0
 
+    if self.loc[1] <= 0:
+      self.loc[1] = self.height
+    elif self.loc[1] > self.height:
+      self.loc[1] = 0 
+    #------------end of testing'''
   def applyF(self, force):
     force /= self.mass
     self.acc = np.add(self.acc, force)
@@ -246,12 +260,14 @@ class Prey:
 # Animats     
 class Predator:
   radius = 30
-
   def __init__(self, x, y):
+    #for testing
+    #self.width = 1000
+    #self.height = 700
+    #-----------end of testing
+    self.timeframe = 0
     self.age = 0 # how long does it live
-
     self.mag = 3
-
     #position
     self.loc = [float(x), float(y)]
     # velocity
@@ -262,19 +278,15 @@ class Predator:
     self.mass = 12
     self.r = self.mass / 2   
 
-
-
     #set default engery
     self.energy = Default_Engery
     self.targetPrey = None
-
 
     #set orientation range in (0 - 359 degrees)
     self.direction = 0
 
     self.touching = None
     self.sees = None
-
     self.behavior = Behavior.stay
 
 
@@ -290,8 +302,24 @@ class Predator:
     # thresholds for deciding an action
     self.move_threshold = 0
 
-  def update(self):
+  def update(self, predators, preys):
+    self.PredForce( preys, predators )
+    self.vel = np.add(self.vel, self.acc)
+    self.loc = np.add(self.loc, self.vel)
+    self.acc = [0., 0.]
+    '''#for testing
+    if self.loc[0] <= 0:
+      self.loc[0] = self.width
+    elif self.loc[0] > self.width:
+      self.loc[0] = 0
 
+    if self.loc[1] <= 0:
+      self.loc[1] = self.height
+    elif self.loc[1] > self.height:
+      self.loc[1] = 0 
+    if self.timeframe%5 == 0:
+      self.record()
+    '''
     ''' self is lion 0
      lion 1 position - self position
      lion 2 position - self position
@@ -304,13 +332,12 @@ class Predator:
      zibra 2 behavior
     '''
 
-    sensors = (
-
-      )
+    sensors = ()
     '''decision = self.net.activate(sensors)'''
     # get a little hungry no matter what
     self.age += 1
     self.get_hungry(self.behavior)
+    self.timeframe += 1
     # move forward
     #self.wants_to_move = (decision[0] > self.move_threshold)
     # rotate left 
@@ -333,5 +360,87 @@ class Predator:
     # inherit parents connection weights
     for i in range(0,len(self.net.params)):
       if random.random() > .05:
-	child.net.params[i] = random.choice([self.net.params[i], other.net.params[i]])
+	       child.net.params[i] = random.choice([self.net.params[i], other.net.params[i]])
     return child
+
+  def applyF(self, force):
+    force /= self.mass
+    self.acc = np.add(self.acc, force)
+
+  '''def avoidForce(self, predators):
+    count = 0
+    locSum = [0., 0.]
+    for p in preys:
+      separation = self.mass + (20*self.mag)
+      dist = np.subtract(p.loc, self.loc)
+      d = np.linalg.norm(dist)
+      if d != 0 and d < separation:
+        locSum = np.add(locSum, p.loc)
+        count += 1
+    if count > 0:
+      locSum /= count
+      avoidVec = np.subtract(self.loc, locSum)
+      #np.linalg.norm()
+      self.applyF(avoidVec)
+  '''
+  def approachForce(self, preys):
+    count = 0
+    locSum = [0., 0.]
+    approachRadius = self.mass + 260
+    captureRadius = 3.0
+    min_dist = sys.float_info.max
+    min_idx = -1
+    #find the closest prey
+    for p in preys:
+      dist = np.subtract(p.loc, self.loc)
+      d = np.linalg.norm(dist)
+      if d <= captureRadius:
+        print d, count
+        print "Capture One Prey"
+      #if d < min_dist and d < approachRadius:
+      if d < min_dist:
+        min_idx = count
+      count+=1
+    #approach the closest prey
+    if min_idx != -1: 
+      approachVec = np.subtract(preys[min_idx].loc,  self.loc)
+      self.applyF(approachVec)
+
+  def avoidForce(self, prads):
+    count = 0
+    locSum = [0., 0.]
+    for p in prads:
+      separation = self.mass + (20*self.mag)
+      dist = np.subtract(p.loc, self.loc)
+      d = np.linalg.norm(dist)
+      if d != 0 and d < separation:
+        locSum = np.add(locSum, p.loc)
+        count += 1
+    if count > 0:
+      locSum /= count
+      avoidVec = np.subtract(self.loc, locSum)
+      #np.linalg.norm()
+      self.applyF(avoidVec)
+
+  def alignForce(self, prads):
+    count = 0
+    velSum = [0., 0.]
+    for p in prads:
+      alignRadius = self.mass + (100*self.mag)
+      dist = np.subtract(p.loc, self.loc)
+      d = np.linalg.norm(dist)
+      if d != 0 and d < alignRadius:
+        velSum = np.add(velSum, p.vel)
+        count += 1
+      if count > 0:
+        velSum /= count
+        alignVec = velSum
+        self.applyF(alignVec)
+
+  def PredForce(self, preys, prads):
+    self.approachForce(preys)
+    #self.avoidForce(prads)
+    #self.alignForce(prads)
+  def record(self):
+    #record current locations
+    return
