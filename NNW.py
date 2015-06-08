bda934130c475c61dc775a1d21dc1a2da61b5a10
tywu <demundo@ucla.edu>
2015-06-08 15:56:28 -0700

    NNW update

--- NNW.py
+++ NNW.py
@@ -8,41 +8,40 @@ from pybrain.datasets import ClassificationDataSet
 from pybrain.datasets import SupervisedDataSet
 
 class NNW:
-	def __init__(self, num_input, num_hidden, num_output):
+  def __init__(self, num_input, num_hidden, num_output):
       # self.net = buildNetwork(num_input, num_hidden, num_output, bias = True)
-      self.net = FeedForwardNetwork()
+    self.net = FeedForwardNetwork()
+    inLayer = LinearLayer(num_input, name='in')
+    hiddenLayer1 = SigmoidLayer(num_hidden, name='hidden1')
+    outLayer = LinearLayer(num_output, name='out')
 
-      inLayer = LinearLayer(num_input, name='in')
-      hiddenLayer1 = SigmoidLayer(num_hidden, name='hidden1')
-      outLayer = LinearLayer(num_output, name='out')
+    self.net.addInputModule(inLayer)
+    self.net.addModule(hiddenLayer1)
+    self.net.addOutputModule(outLayer)
 
-      net.addInputModule(inLayer)
-      net.addModule(hiddenLayer1)
-      net.addOutputModule(outLayer)
+    self.in_to_hidden = FullConnection(inLayer, hiddenLayer1)
+    self.hidden_to_out = FullConnection(hiddenLayer1, outLayer)
 
-      self.in_to_hidden = FullConnection(inLayer, hiddenLayer1)
-      self.hidden_to_out = FullConnection(hiddenLayer1, outLayer)
+    self.net.addConnection(self.in_to_hidden)
+    self.net.addConnection(self.hidden_to_out)
 
-      self.net.addConnection(in_to_hidden)
-      self.net.addConnection(hidden_to_out)
-
-      self.net.sortModules()
+    self.net.sortModules()
 
       # len(self.net.params)
       # len(in_to_hidden.params)
       # len(hidden_to_out.params)
 
 
-    def activate(self, inputData):
+  def activate(self, inputData):
       # self.net.sortModules()
-      decision = self.net.activate(inputDaa)
+      decision = self.net.activate(inputData)
       return decision
 
-    def parameter(laynumber = 0):
-    	if laynumber == 0:
+  def parameter(laynumber = 0):
+    if laynumber == 0:
           return slef.net.params
-        elif laynumber = 1:
+    elif laynumber == 1:
           return self.in_to_hidden.params
-        elif laynumber = 2:
+    elif laynumber == 2:
           return self.hidden_to_out.params
 
